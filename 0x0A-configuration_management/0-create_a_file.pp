e2de1ed31b30e3623ce972196b0f1cc2564cdb5f
Suedreamer <mulingesuzan88@gmail.com>
2023-09-25 14:58:02 -0700

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
-# Create a file in /tmp using Puppet
+# create a file
 
-file { 'holberton':
-  ensure  => present,
-  path    => '/tmp/holberton',
+file { '/tmp/school':
+  content => 'I love Puppet',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet',
 }
