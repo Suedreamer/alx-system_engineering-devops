29511a4a89dbd8c9307bea4eb38cb9249c0415d3
Suedreamer <mulingesuzan88@gmail.com>
2023-09-26 05:29:57 -0700

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# edit config file
+include stdlib
+
+file_line { 'Turn off passwd':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'identity change':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
